c9a94e39acb683b7bd2a77b9c3deca5c148e9166
Joaopedromach <estranhonerd123@gmail.com>
2023-05-08 14:03:51 -0300

    Adicionando metodo POST

--- src/main/kotlin/com/example/demo/controller/ContactController.kt
+++ src/main/kotlin/com/example/demo/controller/ContactController.kt
@@ -4,6 +4,8 @@ import com.example.demo.entities.Contact
 import com.example.demo.repositories.ContactRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,6 +18,10 @@ class ContactController {
     fun index(): List<Contact>{
         return repository.findAll()
     }
+    @PostMapping
+    fun create(@RequestBody contact: Contact): Contact{
+        return repository.save(contact)
 
+    }
 
 }
