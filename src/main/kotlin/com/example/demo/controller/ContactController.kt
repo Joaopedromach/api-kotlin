4efe18f5c122282b2c911e066eee7fe354120a56
Joaopedromach <estranhonerd123@gmail.com>
2023-05-08 14:01:31 -0300

    Adicionando metodo GET

---
+++ src/main/kotlin/com/example/demo/controller/ContactController.kt
@@ -0,0 +1,21 @@
+package com.example.demo.controller
+
+import com.example.demo.entities.Contact
+import com.example.demo.repositories.ContactRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/contacts")
+class ContactController {
+    @Autowired
+    lateinit var repository: ContactRepository
+    @GetMapping
+    fun index(): List<Contact>{
+        return repository.findAll()
+    }
+
+
+}
