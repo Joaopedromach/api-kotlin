7b0cbe3228f6ceb489cddc0338d7f6a9c84cbde0
Joaopedromach <estranhonerd123@gmail.com>
2023-05-09 11:54:52 -0300

    Delete

--- src/main/kotlin/com/example/demo/controller/ContactController.kt
+++ src/main/kotlin/com/example/demo/controller/ContactController.kt
@@ -4,6 +4,7 @@ import com.example.demo.entities.Contact
 import com.example.demo.repositories.ContactRepository
 import jakarta.persistence.EntityNotFoundException
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -28,13 +29,19 @@ class ContactController {
     }
     @PutMapping("/{id}")
     fun update(@PathVariable("id") id:Long,@RequestBody newContact: Contact):Contact{
-        val contact = repository.findById(id).orElseThrow { EntityNotFoundException()}
+        val contact = repository.findById(id).orElseThrow { EntityNotFoundException() }
 
         contact.apply {
             this.name = newContact.name
             this.email = newContact.email
         }
         return repository.save(contact)
+    }
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable("id")id:Long){
+        val contact = repository.findById(id).orElseThrow(){ EntityNotFoundException() }
+        repository.delete(contact)
+
     }
 
 }
